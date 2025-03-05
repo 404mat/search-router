0831e7b66d2fcd8c6880750c06c7854b8b3956b6
Mathias D <mat.decaille@gmail.com>
2025-03-05 14:17:49 -0500

    feat: perplexity + Bang registry

--- src/custom-bangs.ts
+++ src/custom-bangs.ts
@@ -8,4 +8,22 @@ export const customBangs = [
     t: "aic", // meaning "AI Chat"
     u: "https://chat.openai.com?q={{{s}}}", // todo: have a way to set favorite model in local storage
   },
+  {
+    c: "Perplexity",
+    d: "www.perplexity.ai",
+    r: 0,
+    s: "Perplexity",
+    sc: "AI",
+    t: "ppx",
+    u: "https://www.perplexity.ai/search?s=o&q={{{s}}}",
+  },
+  {
+    c: "DuckDuckGo Bangs",
+    d: "www.duckduckgo.com",
+    r: 0,
+    s: "DuckDuckGo Bangs",
+    sc: "",
+    t: "bang",
+    u: "https://duckduckgo.com/bangs?q={{{s}}}",
+  },
 ];
