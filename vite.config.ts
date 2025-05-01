fcd17cb63099dd1626ef7753ed596c36b2532b0d
Mathias D <mat.decaille@gmail.com>
2025-05-01 18:42:55 -0400

    adding back a simpler opensearch config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-import transformPlugin from "vite-plugin-transform";
-import { resolve, join } from "path";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [
-      VitePWA({
-        registerType: "autoUpdate",
-        workbox: {
-          globPatterns: ["**/*.{js,css,html}", "assets/inter*.woff2"],
-        },
-      }),
-    ],
-  };
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html}", "assets/inter*.woff2"],
+      },
+    }),
+  ],
 });
